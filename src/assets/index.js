e939816e7670689a39a5e339c36bb1f65598c970
koechronix <koechronix@yahoo.com>
2023-05-19 20:15:04 +0300

    Added exported data

---
+++ src/assets/index.js
@@ -0,0 +1,79 @@
+import map from './backgrounds/map.jpg';
+import nairobi from './backgrounds/nairobicity.jpg';
+import blackabstract from './backgrounds/black-abstract.jpg';
+import worldmap from './backgrounds/world-map.jpg';
+
+import logo from './logo/logo-black.png';
+import logotext from './logo/logo-text-black.png';
+
+import koech from './personal/koech.png';
+
+import backend from './icons/backend.jpeg';
+import frontend from './icons/frontend.png';
+import github from './icons/github.png';
+import close from './icons/close.jpeg';
+import download from './icons/download.png';
+import downloadHover from './icons/downloadHover.png';
+import pineapple from './icons/pineapple.jpeg';
+import pineappleHover from './icons/pineappleHover.png';
+import menu from './icons/menu.png';
+import resume from './icons/resume.png';
+import send from './icons/send.png';
+import sendHover from './icons/sendHover.png';
+
+import css from './tech/css.png';
+import git from './tech/git.png';
+import html from './tech/html.png';
+import javascript from './tech/javascript.png';
+import nodejs from './tech/nodejs.png';
+import reactjs from './tech/reactjs.png';
+import tailwind from './tech/tailwind.png';
+import sqlite from './tech/sqlite.png';
+import sql from './tech/sql.png';
+import rubyrails from './tech/rubyrails.png';
+
+import learnbuild from './company/learnbuild.jpeg';
+import technobrain from './company/technobrain.jpeg';
+import isandata from './company/isandata.jpeg';
+
+// import komikult from './projects/komikult.png';
+// import leaderboard from './projects/leaderboard.png';
+// import math from './projects/math-magicians.png';
+// import movie from './projects/movie-metro.png';
+// import nyeusi from './projects/nyeusi.png';
+// import space from './projects/space-hub.png';
+
+export {
+  map,
+  nairobi,
+  blackabstract,
+  worldmap,
+  logo,
+  logotext,
+  koech,
+  backend,
+  frontend,
+  github,
+  close,
+  download,
+  downloadHover,
+  pineapple,
+  pineappleHover,
+  menu,
+  resume,
+  send,
+  sendHover,
+  css,
+  git,
+  sqlite,
+  html,
+  javascript,
+  nodejs,
+  sql,
+  rubyrails,
+  reactjs,
+  tailwind,
+  learnbuild,
+  technobrain,
+  isandata,
+};

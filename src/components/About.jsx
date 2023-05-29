e9d7ae09d315b6dd9da93799daccc9329c5cad77
koechronix <koechronix@yahoo.com>
2023-05-29 20:47:05 +0300

    Added an image

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import { motion } from 'framer-motion';
 import { styles } from '../styles';
 import { services } from '../constants';
@@ -11,11 +11,11 @@ const ServiceCard = ({ index, title, icon }) => {
       variants={fadeIn('right', 'spring', 0.5 * index, 0.75)}
       className="xs:w-[250px] w-full card-gradient p-[1px] rounded-[20px] shadow-card">
       <div
-        options={{
-          max: 45,
-          scale: 1,
-          speed: 450,
-        }}
+        // options={{
+        //   max: 45,
+        //   scale: 1,
+        //   speed: 450,
+        // }}
         className="bg-jetLight rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
         <img src={icon} alt={title} className="w-16 h-16 object-contain" />
         <h3 className="text-taupe text-[18px] font-bold text-center">
